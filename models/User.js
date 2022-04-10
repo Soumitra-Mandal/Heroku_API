0719bcc345f01691867db907188dd56e3966361e
Soumitra Mandal <soumitramandal1999@gmail.com>
2022-04-10 13:48:57 +0530

    Update User.js

--- models/User.js
+++ models/User.js
@@ -2,7 +2,7 @@ var mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
     id: Number,
-    name: String, // String is shorthand for {type: String}
+    name: String,
     work: String,
     dialogue: String,
     superhero: Boolean
