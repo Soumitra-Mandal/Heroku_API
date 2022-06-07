dbf1683e056c0659f9b35f47fd5218ce982ffce8
Soumitra Mandal <soumitramandal1999@gmail.com>
2022-06-07 14:31:13 +0530

    Update users.js

--- routes/users.js
+++ routes/users.js
@@ -1,23 +1,23 @@
 var express = require('express');
 var router = express.Router();
 var User = require('../models/User');
-router.get('/', function(req, res, next) {
+router.get('/', function(req, res) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     User.find().then((result) => {
         res.send(result);
     });
 });
-router.post('/', function(req, res, next) {
+router.post('/', function(req, res) {
     res.status(405).send("Post not allowed.");
 });
-router.put('/', function(req, res, next) {
+router.put('/', function(req, res) {
     res.status(405).send("Updation not allowed.");
 });
-router.delete('/', function(req, res, next) {
+router.delete('/', function(req, res) {
     res.status(405).send("Deletion not allowed.");
 });
-router.get('/:id', function(req, res, next) {
+router.get('/:id', function(req, res) {
     const id = req.params.id;
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
@@ -25,13 +25,13 @@ router.get('/:id', function(req, res, next) {
         res.send(result);
     }).catch((err) => res.send(err));
 });
-router.post('/:id', function(req, res, next) {
+router.post('/:id', function(req, res) {
     res.status(405).send("Post not allowed.");
 });
-router.put('/:id', function(req, res, next) {
+router.put('/:id', function(req, res) {
     res.status(405).send("Updation not allowed.");
 });
-router.delete('/:id', function(req, res, next) {
+router.delete('/:id', function(req, res) {
     res.status(405).send("Deletion not allowed.");
 });
 module.exports = router;
